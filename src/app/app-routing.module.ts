f1e8cf4cd5eaf5e94a6240f817c557a30f996861
Jasper Maposa <jaspermaposa@gmail.com>
2019-10-16 15:59:55 +0200

    Added all supporting components and bit of css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', component: PathNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieComponent,
+    MovieDetailComponent,
+    FavoritesComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
